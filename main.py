cd22d362b36d80fe9942dbb0678a8ea5db7079c7
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2026-02-16 17:24:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+# 1
+a = dict()
+print(a)
+
+
+# 2
+talaba = {"ism": "Ali", "yosh": 20, "shahar": "Toshkent"}
+print(talaba)
+
+# 3
+talaba = {"ism": "Sardor",
+          "yosh": 22,
+          "shahar": "Samarqand"
+}
+
+print(talaba)
+print(talaba['ism'])
+
+# 4
+talaba = {"ism": "Malika",
+          "yosh": 19,
+          "shahar": "Buxoro"
+          "telefon" "+998901234567"
+}
+print(talaba)
+
+# 5
+talaba = {"ism": "Jasur",
+          "yosh": 21,
+          "shahar": "Namangan"
+}
+print(talaba)
+talaba["yosh"] = 23
+print(talaba)
+
+# 6
+talaba = {"ism": "Dilnoza",
+          "yosh": 20,
+          "shahar": "Andijon",
+          "kurs": 2
+}
+print(talaba)
+del talaba["shahar"]
+print(talaba)
+
+# 7
+meva = {"olma": 15000,
+        "nok": 12000,
+        "uzum": 20000,
+        "shaftoli": 18000
+}
+print(meva)
+meva.keys()
